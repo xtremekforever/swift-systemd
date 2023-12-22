0f4f9659601e10584b2034beaa25decf34d460fd
Jesse L. Zamora <xtremekforever@gmail.com>
2023-12-22 09:49:47 -0500

    Update example to use notify type service
     - Added code to send the Ready and Stopping signals from the example.

--- Sources/Example/SystemdService.swift
+++ Sources/Example/SystemdService.swift
@@ -5,3 +5,13 @@ if SystemdHelpers.isSystemdService {
 } else {
     print("Not running as a systemd service...")
 }
+
+print("Sending Ready state to systemd...")
+let notifier = SystemdNotifier()
+notifier.notify(ServiceState.Ready)
+
+print("Waiting for a few seconds before exiting...")
+try await Task.sleep(for: .seconds(3))
+
+print("Sending Stopping state to systemd...")
+notifier.notify(ServiceState.Stopping)
