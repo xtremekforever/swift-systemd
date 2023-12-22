2e1272340aa237112509ec490ce9fff7399c419f
Jesse L. Zamora <xtremekforever@gmail.com>
2023-12-22 08:07:48 -0500

    Add simple example, doesn't compile on macOS

--- Package.swift
+++ Package.swift
@@ -14,6 +14,12 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "Systemd")
+        .target(name: "Systemd"),
+        .executableTarget(
+            name: "Example",
+            dependencies: [
+                "Systemd"
+            ]
+        )
     ]
 )
---
+++ Sources/Example/SystemdService.swift
@@ -0,0 +1,7 @@
+import Systemd
+
+if SystemdHelpers.isSystemdService() {
+    print("Running as systemd service!")
+} else {
+    print("Not running as a systemd service...")
+}
