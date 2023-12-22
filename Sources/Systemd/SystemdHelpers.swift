64489e63b49f929808122d2392039e5e36cc67b9
Jesse L. Zamora <xtremekforever@gmail.com>
2023-12-21 20:31:09 -0500

    Add os check in SystemdHelpers
     - If not running in Linux, getIsSystemdService() will return false

--- Sources/Systemd/SystemdHelpers.swift
+++ Sources/Systemd/SystemdHelpers.swift
@@ -1,11 +1,15 @@
 
+#if os(Linux)
 import Glibc
+#endif
+
 import Foundation
 
 struct SystemdHelpers {
     static let isSystemdService: Bool = getIsSystemdService()
 
     private static func getIsSystemdService() -> Bool {
+        #if os(Linux)
         let pid = Glibc.getppid()
         do {
             let name = try String(contentsOfFile: "/proc/\(pid)/comm")
@@ -14,6 +18,8 @@ struct SystemdHelpers {
         } catch {
             print("Unable to determine if running systemd: \(error)")
         }
+        #endif
+
         return false
     }
 }
