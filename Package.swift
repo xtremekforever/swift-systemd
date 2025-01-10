aa6e69e8aff2bd9111741e641fbac9b95d9e74c4
Jesse L. Zamora <xtremekforever@gmail.com>
2025-01-10 16:45:31 -0500

    Set minimum compatibility for Apple platforms

--- Package.swift
+++ Package.swift
@@ -1,10 +1,16 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "swift-systemd",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6),
+    ],
     products: [
         .library(name: "Systemd", targets: ["Systemd"]),
         .library(name: "SystemdLifecycle", targets: ["SystemdLifecycle"]),
